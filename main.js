e03d14096be274dfbad2f974273faa224159e08e
Rishav Sharan <rishav.sharan@live.com>
2013-04-04 18:14:57 +0530

    basic feature for adding stories

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ if (Meteor.isClient) {
     return Book.find({}, {sort: {score: -1, name: 1}});
   };
   
+  Template.templNewStory.events = {
+    'click input.addStory': function () {
+      Book.insert({title: document.getElementById("title").value, 
+                   text: document.getElementById("text").value, 
+                   link: document.getElementById("link").value, 
+                   datetime: new Date,
+                   score: 5, 
+                  });
+    }
+  };
+  
   Template.templStory.events({
     'click input.inc': function () {
       Book.update(this._id, {$inc: {score: 5}});
